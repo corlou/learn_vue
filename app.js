81e70513e1e09f0e8988b26803509d0ea94cfc26
Corina Pelichowski <corina@danitt.com>
2020-07-03 11:36:11 +1000

    adding tasks to list and clear form

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ new Vue({
     data: {
         tasks: [
             { body: 'Go to the store', completed: false }
-        ]
+        ],
+        newTask: ''
+    },
+
+    methods: {
+        addTask: function(e) {
+            e.preventDefault();
+
+            this.tasks.push({
+                body: this.newTask,
+                completed: false
+
+            });
+
+            this.newTask = '';
+        }
     }
-})
+}) 
